2b65572d6f3e9604538160b598dc228c95216aee
vartularora45 <138357352+vartularora45@users.noreply.github.com>
2025-06-17 08:37:10 +0530

    🐛 Fix: Case mismatch in SignUp import

--- .history/frontend/src/App_20250525140512.jsx
+++ .history/frontend/src/App_20250525140512.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import Left from './home/leftpart/Left.jsx';
 import Right from './home/rightpart/Right.jsx';
-import SignUp from './components/SignUp.jsx';
-
+import SignUp from './components/signup.jsx';
 import Login from './components/login.jsx';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth } from './context/AuthContextProvider.jsx';
