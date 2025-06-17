593cd4db12f2a578073eb35bd1599d62e20e210c
vartularora45 <138357352+vartularora45@users.noreply.github.com>
2025-06-17 09:04:54 +0530

    🐛login ko theek kra

--- .history/frontend/src/api_20250617090451.js
+++ .history/frontend/src/api_20250617090451.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL",
   withCredentials: true, // For cookies
   // console.log(`API base URL: ${import.meta.env.VITE_API_URL || "http://localhost:3000"}` 
 });
