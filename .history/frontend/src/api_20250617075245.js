beca78fce22534f63aa3191d2007c5299b71b77e
vartularora45 <138357352+vartularora45@users.noreply.github.com>
2025-06-17 08:33:21 +0530

    🚀 Login feature integrated

--- .history/frontend/src/api_20250617075245.js
+++ .history/frontend/src/api_20250617075245.js
@@ -4,9 +4,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
   withCredentials: true, // For cookies
-  // console.log(`API base URL: ${import.meta.env.VITE_API_URL || "http://localhost:3000"}` 
 });
 
-console.log(`API base URL: ${import.meta.env.VITE_API_URL || "http://localhost:3000"}`);
 
 export default API;
