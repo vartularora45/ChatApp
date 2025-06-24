a63076e62491cdc871369ed7c8d1c54607aeb5e1
vartularora45 <138357352+vartularora45@users.noreply.github.com>
2025-06-24 12:59:06 +0530

    Delete .history directory

--- .history/frontend/src/api_20250616213326.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // 👈 ye hum .env se lenge
-  withCredentials: true, // agar cookies use ho rahi ho to (auth ke liye)
-});
-
-export default API;
--- .history/frontend/src/api_20250616214915.js
+++
@@ -1,9 +0,0 @@
-// src/api.js
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
-  withCredentials: true, // For cookies
-});
-
-export default API;
